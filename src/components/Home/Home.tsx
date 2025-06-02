652aa078d556045f1459f43ddff9da4a112b4a21
lestersanjuan <lsanjua1@uci.edu>
2025-06-01 22:43:27 -0700

    i love my gf

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,9 +3,7 @@ import React from "react";
 export default function Home() {
   return (
     <>
-      <h1>
-        Hello Shift Lead/Supervisor on shift, what would you like to do today?
-      </h1>
+      <h1>Hello Kyven</h1>
       <div>
         <h2>EOW Inventory</h2>
         <h2>Daily Report</h2>
