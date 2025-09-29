dc2072907f1d502710fd4a6ebe79a94bb8ab8f7b
lestersanjuan <lsanjua1@uci.edu>
2025-09-29 01:34:11 -0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -159,7 +159,16 @@ const nuxtConfig = {
   },
 
   router: {
-    middleware: []
+    middleware: [],
+    scrollBehavior(to, from, savedPosition) {
+      if (savedPosition) {
+        return savedPosition
+      }
+      if (to.hash) {
+        return { selector: to.hash, behavior: 'smooth' }
+      }
+      return { x: 0, y: 0 }
+    }
   },
 
   i18n: {
